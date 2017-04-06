668ddf72f8912eede31e3f4e52ea88b8b923303b
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2017-04-06 22:00:42 +0200

    more tests

---
+++ tests/test_output.py
@@ -0,0 +1,21 @@
+from compotia.transpiler.transpiler import Transpiler
+
+transpiler = Transpiler()
+transpiler.transpile('tests/test_project/main.json', 'tests/test_project/output')
+
+
+def test_components():
+    content = ''
+    with open('tests/test_project/output/template/Startpage.html') as _file:
+        content = _file.read()
+    _file.close()
+
+    assert content.count('123') == 2
+
+def test_http_styles():
+    content = ''
+    with open('tests/test_project/output/template/style.css') as _file:
+        content = _file.read()
+    _file.close()
+
+    assert 'Bootstrap' in content
